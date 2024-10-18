9fb0a0d094e26633cb21905e6555c78f23799a7d
Joao-SouzaDev <joao.souzadev@outlook.com>
2024-10-18 12:32:15 -0300

    Projeto iniciado

---
+++ src/main/java/com/erp/gerencialestoque/GerenciamentoDeEstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.erp.gerencialestoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeEstoqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erp/gerencialestoque/GerenciamentoDeEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erp.gerencialestoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
